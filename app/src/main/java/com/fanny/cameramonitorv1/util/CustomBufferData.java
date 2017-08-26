a5fa71e5d26d5b8935fcd9871408acee0949888a
Fanny1021 <happygmf@126.com>
2017-08-26 17:43:05 +0800

    Initial commit

---
+++ app/src/main/java/com/fanny/cameramonitorv1/util/CustomBufferData.java
@@ -0,0 +1,9 @@
+
+package com.fanny.cameramonitorv1.util;
+
+
+
+public class CustomBufferData{
+	public CustomBufferHead head;
+	public byte[] data;
+}
---
+++ app/src/main/java/com/fanny/cameramonitorv1/util/CustomBufferHead.java
@@ -0,0 +1,6 @@
+package com.fanny.cameramonitorv1.util;
+
+public class CustomBufferHead{
+	public int startcode;
+	public int length;
+}
